f0ea081f948e03df6a9626afc5f6c6e5e3624854
Federico Ibarra <federicoibarrab@gmail.com>
2021-11-26 00:15:52 -0300

    Final Refactor con Finder class

--- src/main/java/service/Finder.java
+++ src/main/java/service/Finder.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public class Finder {
     private final Integer MINIMUM_SIZE_OF_LIST_FOR_PROCESS = 1;
-
     private final List<Person> persons;
 
     public Finder(List<Person> persons) {
@@ -19,6 +18,11 @@ public class Finder {
     }
 
     public PersonsAgeComparison findByFinderType(FinderType finderType) {
+        List<PersonsAgeComparison> personsAgeComparisons = getListOfPersonAgeComparisonsFromPersons(persons);
+        return getResult(personsAgeComparisons, finderType);
+    }
+
+    private List<PersonsAgeComparison> getListOfPersonAgeComparisonsFromPersons(List<Person> persons) {
         List<PersonsAgeComparison> personsAgeComparisons = new ArrayList<>();
 
         for (int i = 0; i < persons.size() - 1; i++) {
@@ -26,22 +30,14 @@ public class Finder {
                 personsAgeComparisons.add(getAgeComparisonBetweenPersons(persons.get(i), persons.get(j)));
         }
 
-        return getResult(personsAgeComparisons, finderType);
+        return personsAgeComparisons;
     }
 
     private PersonsAgeComparison getAgeComparisonBetweenPersons(Person firstPerson, Person secondPerson) {
-        Person youngest;
-        Person oldest;
+        if (firstPerson.isYoungestThan(secondPerson))
+            return new PersonsAgeComparison(firstPerson, secondPerson);
 
-        if (firstPerson.isYoungestThan(secondPerson)) {
-            youngest = firstPerson;
-            oldest = secondPerson;
-        } else {
-            youngest = secondPerson;
-            oldest = firstPerson;
-        }
-
-        return new PersonsAgeComparison(youngest, oldest);
+        return new PersonsAgeComparison(secondPerson, firstPerson);
     }
 
     private PersonsAgeComparison getResult(List<PersonsAgeComparison> personsAgeComparisons, FinderType finderType) {
@@ -58,14 +54,14 @@ public class Finder {
         return personsAgeComparisons.size() >= MINIMUM_SIZE_OF_LIST_FOR_PROCESS;
     }
 
-    private PersonsAgeComparison getMinimumAmplitude(List<PersonsAgeComparison> personsAgeComparisons){
+    private PersonsAgeComparison getMinimumAmplitude(List<PersonsAgeComparison> personsAgeComparisons) {
         return Collections.min(
                 personsAgeComparisons,
                 Comparator.comparing(PersonsAgeComparison::getMillisBetweenOldestAndYoungest)
         );
     }
 
-    private PersonsAgeComparison getMaximumAmplitude(List<PersonsAgeComparison> personsAgeComparisons){
+    private PersonsAgeComparison getMaximumAmplitude(List<PersonsAgeComparison> personsAgeComparisons) {
         return Collections.max(
                 personsAgeComparisons,
                 Comparator.comparing(PersonsAgeComparison::getMillisBetweenOldestAndYoungest)
